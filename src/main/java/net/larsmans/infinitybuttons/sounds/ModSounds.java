333ac6403dd6d3448e6b08e21eb81a31d8fca5f8
LarsMans <l.s.vanemmerik@gmail.com>
2022-09-29 17:44:30 +0200

    added sounds

--- src/main/java/net/larsmans/infinitybuttons/sounds/ModSounds.java
+++ src/main/java/net/larsmans/infinitybuttons/sounds/ModSounds.java
@@ -6,17 +6,13 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModSounds {
-    public static SoundEvent ALARM = registerSoundEvent("alarm", 256);
+    public static SoundEvent ALARM = registerSoundEvent("alarm");
     public static SoundEvent STONE_SCRAPE = registerSoundEvent("stone_scrape");
+    public static SoundEvent WOOD_SCRAPE = registerSoundEvent("wood_scrape");
 
     private static SoundEvent registerSoundEvent(String name) {
         Identifier id = new Identifier(InfinityButtonsInit.MOD_ID, name);
         return Registry.register(Registry.SOUND_EVENT, id, new SoundEvent(id));
-    }
-
-    private static SoundEvent registerSoundEvent(String name, float distanceToTravel) {
-        Identifier id = new Identifier(InfinityButtonsInit.MOD_ID, name);
-        return Registry.register(Registry.SOUND_EVENT, id, new SoundEvent((id), distanceToTravel));
     }
 
     public static void registerSounds() {
