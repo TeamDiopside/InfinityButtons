1cb6bacc6257581a09e4ab5f77b56915b1578c3b
LarsMans <l.s.vanemmerik@gmail.com>
2022-10-02 18:54:22 +0200

    sync

--- src/main/java/net/larsmans/infinitybuttons/InfinityButtonsConfigModel.java
+++ src/main/java/net/larsmans/infinitybuttons/InfinityButtonsConfigModel.java
@@ -1,11 +1,16 @@
 package net.larsmans.infinitybuttons;
 
+import io.wispforest.owo.config.Option;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
+import io.wispforest.owo.config.annotation.Sync;
 
 @Modmenu(modId = "infinitybuttons")
 @Config(name = "infinity-buttons-config", wrapperName = "InfinityButtonsConfig")
 public class InfinityButtonsConfigModel {
+
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public boolean alarmSound = true;
+
     public boolean tooltips = true;
 }
