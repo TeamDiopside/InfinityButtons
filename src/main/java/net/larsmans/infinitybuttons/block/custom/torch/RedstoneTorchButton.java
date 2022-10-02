4fad5aa6a44e7b2374da5e5d9d6f3a160a2a345d
LarsMans <l.s.vanemmerik@gmail.com>
2022-10-02 18:50:35 +0200

    added torch buttons and levers

---
+++ src/main/java/net/larsmans/infinitybuttons/block/custom/torch/RedstoneTorchButton.java
@@ -0,0 +1,122 @@
+package net.larsmans.infinitybuttons.block.custom.torch;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TorchBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.DustParticleEffect;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.Nullable;
+
+public class RedstoneTorchButton extends TorchBlock {
+    public static final BooleanProperty LIT = Properties.LIT;
+
+    public RedstoneTorchButton(FabricBlockSettings settings) {
+        super(settings, DustParticleEffect.DEFAULT);
+        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(LIT, false));
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (state.get(LIT)) {
+            return ActionResult.CONSUME;
+        }
+        this.powerOn(state, world, pos);
+        this.playClickSound(player, world, pos, true);
+        world.emitGameEvent((Entity)player, GameEvent.BLOCK_ACTIVATE, pos);
+        return ActionResult.success(world.isClient);
+    }
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (state.get(LIT)) {
+            world.setBlockState(pos, (BlockState)state.with(LIT, false), Block.NOTIFY_ALL);
+            this.updateNeighbors(state, world, pos);
+            this.playClickSound(null, world, pos, false);
+            world.emitGameEvent(null, GameEvent.BLOCK_DEACTIVATE, pos);
+        }
+    }
+
+    public void powerOn(BlockState state, World world, BlockPos pos) {
+        world.setBlockState(pos, (BlockState)state.with(LIT, true), Block.NOTIFY_ALL);
+        this.updateNeighbors(state, world, pos);
+        world.createAndScheduleBlockTick(pos, this, 60);
+    }
+
+    protected void playClickSound(@Nullable PlayerEntity player, WorldAccess world, BlockPos pos, boolean powered) {
+        world.playSound(powered ? player : null, pos, powered ? SoundEvents.BLOCK_WOODEN_BUTTON_CLICK_OFF : SoundEvents.BLOCK_WOODEN_BUTTON_CLICK_ON, SoundCategory.BLOCKS, 0.3f, powered ? 0.6f : 0.5f);
+    }
+
+    public void updateNeighbors(BlockState state, World world, BlockPos pos) {
+        world.updateNeighborsAlways(pos, this);
+    }
+
+    @Override
+    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        for (Direction direction : Direction.values()) {
+            world.updateNeighborsAlways(pos.offset(direction), this);
+        }
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (moved) {
+            return;
+        }
+        for (Direction direction : Direction.values()) {
+            world.updateNeighborsAlways(pos.offset(direction), this);
+        }
+    }
+    @Override
+    public int getStrongRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        if (direction == Direction.DOWN) {
+            return state.getWeakRedstonePower(world, pos, direction);
+        }
+        return 0;
+    }
+
+    @Override
+    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        if (state.get(LIT) && Direction.UP != direction) {
+            return 15;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (state.get(LIT)) {
+            double d = (double) pos.getX() + 0.5 + (random.nextDouble() - 0.5) * 0.2;
+            double e = (double) pos.getY() + 0.7 + (random.nextDouble() - 0.5) * 0.2;
+            double f = (double) pos.getZ() + 0.5 + (random.nextDouble() - 0.5) * 0.2;
+            world.addParticle(this.particle, d, e, f, 0.0, 0.0, 0.0);
+        }
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(LIT);
+    }
+}
---
+++ src/main/java/net/larsmans/infinitybuttons/block/custom/torch/TorchButton.java
@@ -0,0 +1,136 @@
+package net.larsmans.infinitybuttons.block.custom.torch;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.WallTorchBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Util;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.Nullable;
+
+public class TorchButton extends WallTorchBlock {
+
+    public static final BooleanProperty PRESSED = BooleanProperty.of("pressed");
+
+    public TorchButton(FabricBlockSettings settings, ParticleEffect particleEffect) {
+        super(settings, particleEffect);
+        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.NORTH).with(PRESSED, false));
+    }
+
+    @Override
+    public String getTranslationKey() {
+        return Util.createTranslationKey("block", Registry.BLOCK.getId(this));
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        Direction direction = state.get(FACING);
+        if (state.get(PRESSED)) {
+            double d = (double) pos.getX() + 0.5;
+            double e = (double) pos.getY() + 0.6;
+            double f = (double) pos.getZ() + 0.5;
+            Direction direction2 = direction.getOpposite();
+            world.addParticle(ParticleTypes.SMOKE,
+                    d + 0.05 * (double) direction2.getOffsetX(),
+                    e + 0.15,
+                    f + 0.05 * (double) direction2.getOffsetZ(),
+                    0.0, 0.0, 0.0);
+            world.addParticle(this.particle,
+                    d + 0.05 * (double) direction2.getOffsetX(),
+                    e + 0.15,
+                    f + 0.05 * (double) direction2.getOffsetZ(),
+                    0.0, 0.0, 0.0);
+        } else {
+            super.randomDisplayTick(state, world, pos, random);
+        }
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING, PRESSED);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (state.get(PRESSED)) {
+            return ActionResult.CONSUME;
+        }
+        this.powerOn(state, world, pos);
+        this.playClickSound(player, world, pos, true);
+        world.emitGameEvent((Entity)player, GameEvent.BLOCK_ACTIVATE, pos);
+        return ActionResult.success(world.isClient);
+    }
+
+    public void powerOn(BlockState state, World world, BlockPos pos) {
+        world.setBlockState(pos, (BlockState)state.with(PRESSED, true), Block.NOTIFY_ALL);
+        this.updateNeighbors(state, world, pos);
+        world.createAndScheduleBlockTick(pos, this, 60);
+    }
+
+    protected void playClickSound(@Nullable PlayerEntity player, WorldAccess world, BlockPos pos, boolean powered) {
+        world.playSound(powered ? player : null, pos, powered ? SoundEvents.BLOCK_WOODEN_BUTTON_CLICK_OFF : SoundEvents.BLOCK_WOODEN_BUTTON_CLICK_ON, SoundCategory.BLOCKS, 0.3f, powered ? 0.6f : 0.5f);
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (moved || state.isOf(newState.getBlock())) {
+            return;
+        }
+        if (state.get(PRESSED)) {
+            this.updateNeighbors(state, world, pos);
+        }
+        super.onStateReplaced(state, world, pos, newState, moved);
+    }
+
+    @Override
+    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return state.get(PRESSED) ? 15 : 0;
+    }
+
+    @Override
+    public int getStrongRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        if (state.get(PRESSED) && state.get(FACING) == direction) {
+            return 15;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (state.get(PRESSED)) {
+            world.setBlockState(pos, (BlockState)state.with(PRESSED, false), Block.NOTIFY_ALL);
+            this.updateNeighbors(state, world, pos);
+            this.playClickSound(null, world, pos, false);
+            world.emitGameEvent(null, GameEvent.BLOCK_DEACTIVATE, pos);
+        }
+    }
+
+    public void updateNeighbors(BlockState state, World world, BlockPos pos) {
+        world.updateNeighborsAlways(pos, this);
+        world.updateNeighborsAlways(pos.offset(state.get(FACING).getOpposite()), this);
+    }
+}
