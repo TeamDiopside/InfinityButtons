e07a3371189ddc10150b91109662d08eb915ef0d
LarsMans <l.s.vanemmerik@gmail.com>
2022-11-20 11:45:44 +0100

    added actionbar message

--- src/main/java/net/larsmans/infinitybuttons/block/custom/emergencybutton/SafeEmergencyButton.java
+++ src/main/java/net/larsmans/infinitybuttons/block/custom/emergencybutton/SafeEmergencyButton.java
@@ -209,6 +209,7 @@ public class SafeEmergencyButton extends WallMountedBlock {
                     this.openCase(state, world, pos);
                     this.playToggleSound(player, world, pos, true);
                 } else {
+                    player.sendMessage(Text.translatable("infinitybuttons.actionbar.closed_safety_button"), true);
                     return ActionResult.CONSUME;
                 }
             }
