870077add3b96cce17c0837aa1ca9d710c83fceb
LarsMans <l.s.vanemmerik@gmail.com>
2023-05-27 00:59:48 +0200

    fix full hand on safety button

---
+++ src/main/java/net/larsmans/infinitybuttons/mixin/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,34 @@
+package net.larsmans.infinitybuttons.mixin;
+
+import net.larsmans.infinitybuttons.block.custom.emergencybutton.SafeEmergencyButton;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public abstract class ClientPlayerInteractionManagerMixin {
+
+    @Shadow @Final private MinecraftClient client;
+
+    @Inject(method = "interactBlockInternal", at = @At("HEAD"), cancellable = true)
+    private void interactBlockInternal(ClientPlayerEntity player, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
+        World world = this.client.world;
+        BlockState state = world.getBlockState(hitResult.getBlockPos());
+        Block block = state.getBlock();
+        if (block instanceof SafeEmergencyButton) {
+            cir.setReturnValue(block.onUse(state, world, hitResult.getBlockPos(), player, hand, hitResult));
+        }
+    }
+}
---
+++ src/main/java/net/larsmans/infinitybuttons/mixin/ServerPlayerInteractionManagerMixin.java
@@ -0,0 +1,29 @@
+package net.larsmans.infinitybuttons.mixin;
+
+import net.larsmans.infinitybuttons.block.custom.emergencybutton.SafeEmergencyButton;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ServerPlayerInteractionManager.class)
+public abstract class ServerPlayerInteractionManagerMixin {
+
+    @Inject(method = "interactBlock", at = @At("HEAD"), cancellable = true)
+    public void interactBlockInternal(ServerPlayerEntity player, World world, ItemStack stack, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
+        BlockState state = world.getBlockState(hitResult.getBlockPos());
+        Block block = state.getBlock();
+        if (block instanceof SafeEmergencyButton) {
+            cir.setReturnValue(block.onUse(state, world, hitResult.getBlockPos(), player, hand, hitResult));
+        }
+    }
+}
