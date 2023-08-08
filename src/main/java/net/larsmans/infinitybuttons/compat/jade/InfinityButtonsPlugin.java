4991f80418b7d2551cfc204419d230032ab75aa2
curryducker <jur.hoekman@gmail.com>
2023-08-08 21:40:29 +0200

    bookshelf power

--- src/main/java/net/larsmans/infinitybuttons/compat/jade/InfinityButtonsPlugin.java
+++ src/main/java/net/larsmans/infinitybuttons/compat/jade/InfinityButtonsPlugin.java
@@ -3,14 +3,21 @@ package net.larsmans.infinitybuttons.compat.jade;
 import net.larsmans.infinitybuttons.InfinityButtonsClientInit;
 import net.larsmans.infinitybuttons.InfinityButtonsInit;
 import net.larsmans.infinitybuttons.block.custom.secretbutton.AbstractSecretButton;
+import net.larsmans.infinitybuttons.block.custom.secretbutton.BookshelfSecretButton;
 import net.larsmans.infinitybuttons.block.custom.torch.RedstoneTorchButton;
 import net.larsmans.infinitybuttons.block.custom.torch.TorchButton;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
 import snownee.jade.api.config.IWailaConfig;
+import snownee.jade.api.platform.CustomEnchantPower;
 
 @WailaPlugin
 public class InfinityButtonsPlugin implements IWailaPlugin {
@@ -31,6 +38,11 @@ public class InfinityButtonsPlugin implements IWailaPlugin {
         registration.addConfig(CONFIG_HIDE_TORCH_BUTTONS, true);
         registration.markAsClientFeature(CONFIG_HIDE_SECRET_BUTTONS);
         registration.markAsClientFeature(CONFIG_HIDE_TORCH_BUTTONS);
+
+        for (Block block : Registries.BLOCK)
+            if (block instanceof BookshelfSecretButton)
+                registration.registerCustomEnchantPower(block, (blockState, world, blockPos) -> 1);
+
         registration.addRayTraceCallback((hitResult, accessor, originalAccessor) -> {
             if (accessor instanceof BlockAccessor blockAccessor) {
                 if (hidden(CONFIG_HIDE_SECRET_BUTTONS) && blockAccessor.getBlock() instanceof AbstractSecretButton secretButton) {
