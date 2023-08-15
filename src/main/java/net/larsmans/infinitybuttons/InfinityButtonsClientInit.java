12847e8d64f14873bfa5dbb416d159ca370c63db
curryducker <jur.hoekman@gmail.com>
2023-08-15 20:23:19 +0200

    global sound

--- src/main/java/net/larsmans/infinitybuttons/InfinityButtonsClientInit.java
+++ src/main/java/net/larsmans/infinitybuttons/InfinityButtonsClientInit.java
@@ -114,8 +114,10 @@ public class InfinityButtonsClientInit implements ClientModInitializer {
 
     public static void playGlobalSound(World world, BlockPos pos, SoundEvent soundEvent, SoundCategory soundCategory) {
         Camera cam = MinecraftClient.getInstance().gameRenderer.getCamera();
+        Vec3d centerPos = pos.toCenterPos();
         if (cam.isReady()) {
-            world.playSound(pos.getX(), pos.getY(), pos.getZ(), soundEvent, soundCategory, (float)cam.getPos().distanceTo(Vec3d.ofCenter(pos))/16.0F + 20.0F, 1.0F, false);
+            float chunkDistance = (float)cam.getPos().distanceTo(centerPos) / 16.0f;
+            world.playSound(centerPos.getX(), centerPos.getY(), centerPos.getZ(), soundEvent, soundCategory, chunkDistance * 1.3f + 20f, 1.0f, false);
         }
     }
 
