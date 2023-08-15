20bc020a95cfa2f482a4ef906d6b70258d427982
curryducker <jur.hoekman@gmail.com>
2023-08-15 20:07:30 +0200

    hopefully fix advancements bug

--- src/main/java/net/larsmans/infinitybuttons/item/SafeEmergencyButtonItem.java
+++ src/main/java/net/larsmans/infinitybuttons/item/SafeEmergencyButtonItem.java
@@ -4,10 +4,13 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.larsmans.infinitybuttons.advancement.InfinityButtonsTriggers;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class SafeEmergencyButtonItem extends BlockItem {
     public SafeEmergencyButtonItem(Block block, FabricItemSettings settings) {
@@ -16,7 +19,7 @@ public class SafeEmergencyButtonItem extends BlockItem {
 
     @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-        if (entity instanceof ServerPlayerEntity && slot == 3) {
+        if (entity instanceof ServerPlayerEntity serverPlayerEntity && Objects.equals(serverPlayerEntity.getEquippedStack(EquipmentSlot.HEAD).getItem(), this)) {
             InfinityButtonsTriggers.SAFETY_TRIGGER.trigger((ServerPlayerEntity) entity);
         }
     }
