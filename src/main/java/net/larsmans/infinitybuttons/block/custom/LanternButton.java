afe1de1ea2d8c24ab81b7596ec50cf8e301a1d36
curryducker <jur.hoekman@gmail.com>
2023-08-21 10:45:38 +0200

    imports

--- src/main/java/net/larsmans/infinitybuttons/block/custom/LanternButton.java
+++ src/main/java/net/larsmans/infinitybuttons/block/custom/LanternButton.java
@@ -1,4 +1,5 @@
 package net.larsmans.infinitybuttons.block.custom;
+
 import net.larsmans.infinitybuttons.InfinityButtonsUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -18,7 +19,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.ChainBlock;
 import net.minecraft.world.level.block.LanternBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
--- src/main/java/net/larsmans/infinitybuttons/mixin/ChainBlockMixin.java
+++ src/main/java/net/larsmans/infinitybuttons/mixin/ChainBlockMixin.java
@@ -3,7 +3,6 @@ package net.larsmans.infinitybuttons.mixin;
 import net.larsmans.infinitybuttons.block.custom.LanternButton;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
