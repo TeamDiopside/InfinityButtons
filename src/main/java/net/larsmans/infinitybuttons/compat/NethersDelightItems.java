fec6404a17f62c8c37fba1bb861d5e8754f57b07
curryducker <jur.hoekman@gmail.com>
2023-08-23 11:29:54 +0200

    registry fix

--- src/main/java/net/larsmans/infinitybuttons/compat/NethersDelightItems.java
+++ src/main/java/net/larsmans/infinitybuttons/compat/NethersDelightItems.java
@@ -2,19 +2,33 @@ package net.larsmans.infinitybuttons.compat;
 
 import net.larsmans.infinitybuttons.InfinityButtons;
 import net.larsmans.infinitybuttons.item.InfinityButtonsItems;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.StandingAndWallBlockItem;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+import static net.larsmans.infinitybuttons.item.InfinityButtonsItems.ITEMS;
 
 public class NethersDelightItems {
 
     public static final RegistryObject<Item> PROPELPLANT_TORCH_BUTTON =
-            InfinityButtonsItems.registerTorch("propelplant_torch_button", NethersDelightBlocks.PROPELPLANT_TORCH_BUTTON, NethersDelightBlocks.PROPELPLANT_WALL_TORCH_BUTTON);
+            registerItem("propelplant_torch_button",
+                    () -> new StandingAndWallBlockItem(NethersDelightBlocks.PROPELPLANT_TORCH_BUTTON.get(), NethersDelightBlocks.PROPELPLANT_WALL_TORCH_BUTTON.get(),
+                            new Item.Properties(), Direction.DOWN));
     public static final RegistryObject<Item> PROPELPLANT_TORCH_LEVER =
-            InfinityButtonsItems.registerTorch("propelplant_torch_lever", NethersDelightBlocks.PROPELPLANT_TORCH_LEVER, NethersDelightBlocks.PROPELPLANT_WALL_TORCH_LEVER);
+            registerItem("propelplant_torch_lever",
+                    () -> new StandingAndWallBlockItem(NethersDelightBlocks.PROPELPLANT_TORCH_LEVER.get(), NethersDelightBlocks.PROPELPLANT_WALL_TORCH_LEVER.get(),
+                            new Item.Properties(), Direction.DOWN));
 
     /**
      * Methods
      */
+
+    private static <T extends Item> RegistryObject<T> registerItem(String name, Supplier<T> item) {
+        return ITEMS.register(name, item);
+    }
 
     public static void registerCompatItems() {
         InfinityButtons.LOGGER.debug("Registering Nether's Delight Compat Items for Infinity Buttons");
--- src/main/java/net/larsmans/infinitybuttons/item/InfinityButtonsItems.java
+++ src/main/java/net/larsmans/infinitybuttons/item/InfinityButtonsItems.java
@@ -21,20 +21,28 @@ public class InfinityButtonsItems {
      * Torches
      */
 
-    public static final RegistryObject<Item> TORCH_BUTTON = registerTorch("torch_button", InfinityButtonsBlocks.TORCH_BUTTON, InfinityButtonsBlocks.WALL_TORCH_BUTTON);
-    public static final RegistryObject<Item> TORCH_LEVER = registerTorch("torch_lever", InfinityButtonsBlocks.TORCH_LEVER, InfinityButtonsBlocks.WALL_TORCH_LEVER);
-    public static final RegistryObject<Item> SOUL_TORCH_BUTTON = registerTorch("soul_torch_button", InfinityButtonsBlocks.SOUL_TORCH_BUTTON, InfinityButtonsBlocks.SOUL_WALL_TORCH_BUTTON);
-    public static final RegistryObject<Item> SOUL_TORCH_LEVER = registerTorch("soul_torch_lever", InfinityButtonsBlocks.SOUL_TORCH_LEVER, InfinityButtonsBlocks.SOUL_WALL_TORCH_LEVER);
-    public static final RegistryObject<Item> REDSTONE_TORCH_BUTTON = registerTorch("redstone_torch_button", InfinityButtonsBlocks.REDSTONE_TORCH_BUTTON, InfinityButtonsBlocks.REDSTONE_WALL_TORCH_BUTTON);
-    public static final RegistryObject<Item> REDSTONE_TORCH_LEVER = registerTorch("redstone_torch_lever", InfinityButtonsBlocks.REDSTONE_TORCH_LEVER, InfinityButtonsBlocks.REDSTONE_WALL_TORCH_LEVER);
+    public static final RegistryObject<Item> TORCH_BUTTON = registerItem("torch_button",
+            () -> new StandingAndWallBlockItem(InfinityButtonsBlocks.TORCH_BUTTON.get(), InfinityButtonsBlocks.WALL_TORCH_BUTTON.get(),
+                    new Item.Properties(), Direction.DOWN));
+    public static final RegistryObject<Item> TORCH_LEVER = registerItem("torch_lever",
+            () -> new StandingAndWallBlockItem(InfinityButtonsBlocks.TORCH_LEVER.get(), InfinityButtonsBlocks.WALL_TORCH_LEVER.get(),
+                    new Item.Properties(), Direction.DOWN));
+    public static final RegistryObject<Item> SOUL_TORCH_BUTTON = registerItem("soul_torch_button",
+            () -> new StandingAndWallBlockItem(InfinityButtonsBlocks.SOUL_TORCH_BUTTON.get(), InfinityButtonsBlocks.SOUL_WALL_TORCH_BUTTON.get(),
+                    new Item.Properties(), Direction.DOWN));
+    public static final RegistryObject<Item> SOUL_TORCH_LEVER = registerItem("soul_torch_lever",
+            () -> new StandingAndWallBlockItem(InfinityButtonsBlocks.SOUL_TORCH_LEVER.get(), InfinityButtonsBlocks.SOUL_WALL_TORCH_LEVER.get(),
+                    new Item.Properties(), Direction.DOWN));
+    public static final RegistryObject<Item> REDSTONE_TORCH_BUTTON = registerItem("redstone_torch_button",
+            () -> new StandingAndWallBlockItem(InfinityButtonsBlocks.REDSTONE_TORCH_BUTTON.get(), InfinityButtonsBlocks.REDSTONE_WALL_TORCH_BUTTON.get(),
+                    new Item.Properties(), Direction.DOWN));
+    public static final RegistryObject<Item> REDSTONE_TORCH_LEVER = registerItem("redstone_torch_lever",
+            () -> new StandingAndWallBlockItem(InfinityButtonsBlocks.REDSTONE_TORCH_LEVER.get(), InfinityButtonsBlocks.REDSTONE_WALL_TORCH_LEVER.get(),
+                    new Item.Properties(), Direction.DOWN));
 
     /**
      * Methods
      */
-
-    public static RegistryObject<Item> registerTorch(String name, RegistryObject<Block> standing, RegistryObject<Block> wall) {
-        return registerItem(name, () -> new StandingAndWallBlockItem(standing.get(), wall.get(), new Item.Properties(), Direction.DOWN));
-    }
 
     private static <T extends Item> RegistryObject<T> registerItem(String name, Supplier<T> item) {
         return ITEMS.register(name, item);
