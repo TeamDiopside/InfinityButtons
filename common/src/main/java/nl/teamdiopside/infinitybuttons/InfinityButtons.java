9304ae870d76497ca08d366d5bd34bdc46eba721
PhantomEye <armand.devries@outlook.com>
2025-10-14 21:30:14 +0200

    Assets & Data copied over from 1.20, minus things that should be generated

--- common/src/main/java/nl/teamdiopside/infinitybuttons/InfinityButtons.java
+++ common/src/main/java/nl/teamdiopside/infinitybuttons/InfinityButtons.java
@@ -1,7 +1,7 @@
 package nl.teamdiopside.infinitybuttons;
 
 public final class InfinityButtons {
-    public static final String MOD_ID = "infinitybuttons";
+    public static final String MOD_ID = "assets/infinitybuttons";
 
     public static void init() {
         // Write common init code here.
