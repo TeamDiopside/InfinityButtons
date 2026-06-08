465e5a8ff4bfa944658152e550c12a5f3345b26e
phantomeye1931 <armand.devries@outlook.com>
2026-06-08 23:42:32 +0200

    Neapolitan compat

---
+++ common/src/main/java/nl/teamdiopside/infinitybuttons/compat/blocks/NeapolitanBlocks.java
@@ -0,0 +1,32 @@
+package nl.teamdiopside.infinitybuttons.compat.blocks;
+
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import nl.teamdiopside.infinitybuttons.block.faced4.SecretButton;
+import nl.teamdiopside.infinitybuttons.block.faced4.SecretButtonType;
+import nl.teamdiopside.infinitybuttons.compat.IBModdedBlocks;
+
+import static com.mojang.text2speech.Narrator.LOGGER;
+
+public class NeapolitanBlocks extends IBModdedBlocks {
+    public static final NeapolitanBlocks INSTANCE = new NeapolitanBlocks();
+    public static final String NAMESPACE = "neapolitan";
+
+    private static final BlockBehaviour.Properties properties = BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BROWN)
+            .ignitedByLava().strength(2.0f, 3.0f).noOcclusion().sound(SoundType.WOOD).requiresCorrectToolForDrops();
+
+    public static final RegistrySupplier<SecretButton> CHISELED_CHOCOLATE_BRICK_SECRET_BUTTON = INSTANCE.registerSecretButton("chiseled_chocolate_brick_secret_button",
+            SecretButtonType.CHISELED_STONE_BRICK, properties, ResourceLocation.fromNamespaceAndPath(NAMESPACE, "chiseled_chocolate_bricks"));
+
+    private NeapolitanBlocks() {
+        super(NAMESPACE);
+    }
+
+    @Override
+    public void registerMine() {
+        LOGGER.info("Infinity Buttons: Registering Neapolitan buttons");
+    }
+}
