16ca8a64c3376f8bfa5d505c6d114d4bf8c35d8b
Curry <66020230+curryducker@users.noreply.github.com>
2026-09-21 23:40:15 +0200

    Improve Emergency Button Constructor

--- common/src/main/java/nl/teamdiopside/infinitybuttons/block/emergency/SafeEmergencyButton.java
+++ common/src/main/java/nl/teamdiopside/infinitybuttons/block/emergency/SafeEmergencyButton.java
@@ -12,7 +12,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.AttachFace;
-import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.phys.BlockHitResult;
@@ -33,7 +32,7 @@ public class SafeEmergencyButton extends EmergencyButton {
     protected final BiHashMap<Direction, AttachFace, VoxelShape> SHAPES_CLOSED = new BiHashMap<>();
 
     public SafeEmergencyButton(Properties properties) {
-        super(BlockSetType.STONE, properties, SHAPE_PRESSED, SHAPE_OPEN);
+        super(properties, SHAPE_PRESSED, SHAPE_OPEN);
 
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(FACING, Direction.NORTH)
