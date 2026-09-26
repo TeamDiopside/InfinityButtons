104fa18cfa9073ae69af7fc02d4c49cfb18dc93c
Curry <66020230+curryducker@users.noreply.github.com>
2026-09-26 11:28:06 +0200

    Change structure of jammed button interaction

--- common/src/main/java/nl/teamdiopside/infinitybuttons/block/faced6/normal/JammedButton.java
+++ common/src/main/java/nl/teamdiopside/infinitybuttons/block/faced6/normal/JammedButton.java
@@ -3,7 +3,9 @@ package nl.teamdiopside.infinitybuttons.block.faced6.normal;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MaceItem;
@@ -21,15 +23,19 @@ public class JammedButton extends NormalButton {
 
     @Override
     protected @NotNull InteractionResult useWithoutItem(BlockState blockState, Level level, BlockPos blockPos, Player player, BlockHitResult blockHitResult) {
+        player.displayClientMessage(Component.translatable("infinitybuttons.actionbar.jammed_button"), true);
+        return InteractionResult.CONSUME;
+    }
+
+    @Override
+    protected @NotNull ItemInteractionResult useItemOn(ItemStack itemStack, BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
         ItemStack inHand = player.getItemInHand(player.getUsedItemHand());
         if (inHand.getItem() instanceof MaceItem) {
             this.press(blockState, level, blockPos, player);
             level.playSound(null, blockPos.getX(), blockPos.getY(), blockPos.getZ(), SoundEvents.MACE_SMASH_AIR, player.getSoundSource(), 1.0F, 1.0F);
             spawnDestroyParticles(level, player, blockPos, blockState);
-            return InteractionResult.SUCCESS;
+            return ItemInteractionResult.SUCCESS;
         }
-
-        player.displayClientMessage(Component.translatable("infinitybuttons.actionbar.jammed_button"), true);
-        return InteractionResult.CONSUME;
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
 }
